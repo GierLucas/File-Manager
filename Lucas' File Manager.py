20741efbf31b6152a3dfd11498ba3479d9430d13
GierLucas <33908879+GierLucas@users.noreply.github.com>
2022-08-10 16:14:20 +0200

    Update Lucas' File Manager.py

--- Lucas' File Manager.py
+++ Lucas' File Manager.py
@@ -34,7 +34,27 @@ def copy_file():
                 messagebox.showinfo(title="File Copied", message="The file was succesfully copied!")
         except OSError as copyFileError:
             messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(copyFileError) + "\n\nPlease try again!")
-            
+
+def copy_folder():
+    messagebox.showinfo(title="Copy Folder", message="Select the folder you want to copy!")
+    copyFolderSource = filedialog.askdirectory()
+    if copyFolderSource == "":
+        messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
+    else:
+        try:
+            messagebox.showinfo(title="Copy Folder", message="Select the location you want to copy the folder to!")
+            copyFileDestination = filedialog.askdirectory()
+            if copyFileDestination == "":
+                messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
+            else:
+                copyFolderSourceSplit = os.path.split(copyFolderSource)
+                joinedPath = os.path.join(copyFileDestination, copyFolderSourceSplit[1])
+                os.mkdir(joinedPath)
+                copy_tree(copyFolderSource, joinedPath)
+                messagebox.showinfo(title="Folder Copied", message="The folder was succesfully copied")
+        except IOError as copyFolderError:
+            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(copyFolderError) + "\n\nPlease try again!")
+
 def delete_file():
     messagebox.showinfo(title="Delete File", message="Select the file you want to delete!")
     fileNameDelete = filedialog.askopenfilename()
@@ -46,6 +66,52 @@ def delete_file():
             messagebox.showinfo(title="File Deleted", message="The file was succesfully deleted!")
         except OSError as deleteFileError:
             messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(deleteFileError) + "\n\nPlease try again!")
+
+def delete_folder():
+    messagebox.showinfo(title="Delete Folder", message="Select the folder you want to delete!")
+    deleteFolderName = filedialog.askdirectory()
+    if deleteFolderName == "":
+        messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
+    else:
+        try:
+            shutil.rmtree(deleteFolderName)
+            messagebox.showinfo(title="Folder Deleted", message="The folder was succesfully deleted!")
+        except OSError as deleteFolderError:
+            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(deleteFolderError) + "\n\nPlease try again!")
+
+def move_file():
+    messagebox.showinfo(title="Move File", message="Select the file you want to move!")
+    moveFileSource = filedialog.askopenfilename()
+    if moveFileSource == "":
+        messagebox.showinfo(title="File Select Canceled", message="You have canceled the file selection!")
+    else:
+        try:
+            messagebox.showinfo(title="Move File", message="Select the location you want to move the file to!")
+            moveFileDestination = filedialog.askdirectory()
+            if moveFileDestination == "":
+                messagebox.showinfo(title="File Select Canceled", message="You have canceled the file selection!")
+            else:
+                shutil.move(moveFileSource, moveFileDestination)
+                messagebox.showinfo(title="File Moved", message="The file has been succesfully moved!")
+        except OSError as moveFileError:
+            messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(moveFileError) + "\n\nPlease try again!")
+
+def move_folder():
+    messagebox.showinfo(title="Move Folder", message="Select the folder you want to move!")
+    moveFolderSource = filedialog.askdirectory()
+    if moveFolderSource == "":
+        messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
+    else:
+        try:
+            messagebox.showinfo(title="Move Folder", message="Select the location you want to move the folder to!")
+            moveFolderDestination = filedialog.askdirectory()
+            if moveFolderDestination == "":
+                messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
+            else:
+                shutil.move(moveFolderSource, moveFolderDestination)
+                messagebox.showinfo(title="Folder Moved", message="The folder has been succesfully moved!")
+        except OSError as moveFolderError:
+            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(moveFolderError) + "\n\nPlease try again!")
 
 def rename_file():
     messagebox.showinfo(title="Rename File", message="Select the file you want to rename!")
@@ -68,35 +134,6 @@ def rename_file():
                 messagebox.showinfo(title="File Renamed", message="The file has succesfully been renamed!")
         except OSError as renameFileError:
             messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(renameFileError) + "\n\nPlease try again!")
-
-def move_file():
-    messagebox.showinfo(title="Move File", message="Select the file you want to move!")
-    moveFileSource = filedialog.askopenfilename()
-    if moveFileSource == "":
-        messagebox.showinfo(title="File Select Canceled", message="You have canceled the file selection!")
-    else:
-        try:
-            messagebox.showinfo(title="Move File", message="Select the location you want to move the file to!")
-            moveFileDestination = filedialog.askdirectory()
-            if moveFileDestination == "":
-                messagebox.showinfo(title="File Select Canceled", message="You have canceled the file selection!")
-            else:
-                shutil.move(moveFileSource, moveFileDestination)
-                messagebox.showinfo(title="File Moved", message="The file has been succesfully moved!")
-        except OSError as moveFileError:
-            messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(moveFileError) + "\n\nPlease try again!")
-
-def delete_folder():
-    messagebox.showinfo(title="Delete Folder", message="Select the folder you want to delete!")
-    deleteFolderName = filedialog.askdirectory()
-    if deleteFolderName == "":
-        messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
-    else:
-        try:
-            shutil.rmtree(deleteFolderName)
-            messagebox.showinfo(title="Folder Deleted", message="The folder was succesfully deleted!")
-        except OSError as deleteFolderError:
-            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(deleteFolderError) + "\n\nPlease try again!")
 
 def create_folder():
     messagebox.showinfo(title="Create Folder", message="Select the location where you want to create the folder!")
@@ -131,41 +168,23 @@ def listFiles_folder():
             messagebox.showinfo(title="Files Listed", message="The files from the selected folder were succesfully listed!")
             print(listFilesFolder)
         except OSError as listFilesError:
-            messagebox.showerror(title="File Error", message="The following error has occured:\n\n"+ str(listFilesError) + "\n\nPlease try again!")
-
-def copy_folder():
-    messagebox.showinfo(title="Copy Folder", message="Select the folder you want to copy!")
-    copyFolderSource = filedialog.askdirectory()
-    if copyFolderSource == "":
-        messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
-    else:
-        try:
-            messagebox.showinfo(title="Copy Folder", message="Select the location you want to copy the folder to!")
-            copyFileDestination = filedialog.askdirectory()
-            if copyFileDestination == "":
-                messagebox.showinfo(title="Folder Select Canceled", message="You have canceled the folder selection!")
-            else:
-                copyFolderSourceSplit = os.path.split(copyFolderSource)
-                joinedPath = os.path.join(copyFileDestination, copyFolderSourceSplit[1])
-                os.mkdir(joinedPath)
-                copy_tree(copyFolderSource, joinedPath)
-                messagebox.showinfo(title="Folder Copied", message="The folder was succesfully copied")
-        except IOError as copyFolderError:
-            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(copyFolderError) + "\n\nPlease try again!")
+            messagebox.showerror(title="Folder Error", message="The following error has occured:\n\n"+ str(listFilesError) + "\n\nPlease try again!")
 
 def gui_window():
     guiWindow = tkinter.Tk()
+
     tkinter.Label(guiWindow, text="Lucas' Files Manager\n", fg="green", font=("", 40, "normal", "underline")).pack()
     
     tkinter.Button(guiWindow, text="Open File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=open_file).pack()
     tkinter.Button(guiWindow, text="Copy File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=copy_file).pack()
-    tkinter.Button(guiWindow, text="Delete File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=delete_file).pack()
-    tkinter.Button(guiWindow, text="Rename File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=rename_file).pack()
-    tkinter.Button(guiWindow, text="Move File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=move_file).pack()
-    tkinter.Button(guiWindow, text="Delete Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=delete_folder).pack()
-    tkinter.Button(guiWindow, text="Create Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=create_folder).pack()
     tkinter.Button(guiWindow, text="Copy Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=copy_folder).pack()
-    tkinter.Button(guiWindow, text="List all files in selected folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=listFiles_folder).pack()
+    tkinter.Button(guiWindow, text="Delete File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=delete_file).pack()
+    tkinter.Button(guiWindow, text="Delete Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=delete_folder).pack()
+    tkinter.Button(guiWindow, text="Move File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=move_file).pack()
+    tkinter.Button(guiWindow, text="Move Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=move_folder).pack()
+    tkinter.Button(guiWindow, text="Rename File", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=rename_file).pack()
+    tkinter.Button(guiWindow, text="Create Folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=create_folder).pack()
+    tkinter.Button(guiWindow, text="List all files in folder", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=listFiles_folder).pack()
     tkinter.Button(guiWindow, text="Exit Program", width=25, height=1, bg="blue", fg="red", font=("", "20", "bold"), command=guiWindow.destroy).pack()
 
     guiWindow.mainloop()
